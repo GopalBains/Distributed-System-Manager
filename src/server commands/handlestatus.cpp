c3a1c54979304d5c6e5a9164859a16f7edfd63ed
Gopal Bains <149717529+GopalBains@users.noreply.github.com>
2026-05-06 23:37:26 +0530

    Create handlestatus.cpp

---
+++ src/server commands/handlestatus.cpp
@@ -0,0 +1,18 @@
+#include "../../headers/serverService.hpp"
+#include "../../headers/Status_codes.hpp"
+#include "../../headers/sockets.hpp"
+
+void *handle_status_check(void *arg)
+{
+    TCP socket = *(TCP *)arg;
+    if(OPEN_RECEIVE_FILE_CONNECTION)
+    {
+        socket.sendData(STATUS_MESSAGES[OPEN_SHAREFILE_CONNECTION], strlen(STATUS_MESSAGES[OPEN_SHAREFILE_CONNECTION]));
+    }
+    else
+    {
+        socket.sendData(STATUS_MESSAGES[SUCCESS], strlen(STATUS_MESSAGES[SUCCESS]));
+    }
+    delete (TCP *)arg;
+    return NULL;
+}
